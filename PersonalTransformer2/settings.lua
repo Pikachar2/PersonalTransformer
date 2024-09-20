919d1b8661405002cd68a0e0a5d9da96c1628c66
Pikachar2 <highiq2@hotmail.com>
2024-09-20 10:50:48 -0600

    Added setting to control tick rate

--- PersonalTransformer2/settings.lua
+++ PersonalTransformer2/settings.lua
@@ -7,5 +7,15 @@ data:extend
     default_value = false,
     per_user = false,
     order = "a1"
+  },
+  {
+    type = "int-setting",
+    name = "personal-transformer2-tick-delay",
+    setting_type = "runtime-global",
+	minimum_value = 1,
+	maximum_value = 60,
+    default_value = 1,
+    per_user = false,
+    order = "a2"
   }
 }
